b12e4d888cc38ee78b817312682351ed7524604b
Srivalli-1 <kajasrivalli5@gmail.com>
2025-05-11 19:01:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+npm start
+
+> ca-retake-practice@1.0.0 start
+> node server.js
+
+listening
+Cannot read properties of undefined (reading 'length')
+Cannot read properties of undefined (reading 'length')
+Cannot read properties of undefined (reading 'length')
